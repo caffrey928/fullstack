76e4af5c30e4875ef45d38034ee299f194fbaf82
caffrey928 <caffreyacnologialawace@gmail.com>
2024-03-30 23:54:52 +0800

    change frontend fetch url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://fullstack-9avnu74or-caffrey928s-projects.vercel.app/",
+        target: "https://fullstack-d34ixky4y-caffrey928s-projects.vercel.app/",
         changeOrigin: true,
       },
     },
