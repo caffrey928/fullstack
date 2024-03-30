082d19aaa613ec5540a801b6b461b41ceb270b20
caffrey928 <caffreyacnologialawace@gmail.com>
2024-03-30 22:50:51 +0800

    change vite.config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   root: './',
   build: {
-    outDir: './dist'
+    outDir: './public'
   },
   plugins: [react()],
   server: {
