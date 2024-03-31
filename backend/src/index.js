fda8c083244cd4797a04b60e1e70a911d63d544f
caffrey928 <caffreyacnologialawace@gmail.com>
2024-03-31 17:43:55 +0800

    add default html to backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,6 +5,7 @@ const cookieParser = require("cookie-parser");
 const { prisma } = require("./adapters.js");
 const rootRouter = require("./routes/index.js");
 const { csrfErrorHandler, doubleCsrfProtection } = require("./csrf.js");
+const path = require("path");
 // import path, { dirname } from "path";
 // import { fileURLToPath } from "url";
 const createMemoryStore = require("memorystore");
@@ -18,6 +19,7 @@ const port = process.env.PORT || 8000;
 const app = express();
 
 // app.use(express.static(frontendDir));
+app.use(express.static('public'));
 
 if (process.env.NODE_ENV === "production") {
   app.set("trust proxy", 1);
@@ -52,7 +54,7 @@ app.use(rootRouter);
 
 app.get("*", (req, res) => {
   if (!req.originalUrl.startsWith("/api")) {
-    // return res.sendFile(path.join(frontendDir, "index.html"));
+    // return res.sendFile(path.join(__dirname, ".", "index.html"));
     return "Hello World!!"
   }
   return res.status(404).send();
